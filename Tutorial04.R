a27b92f6346fd45792e16ec41525261ceec13e76
LeSean Bruneau <anyone@hotmai.com>
2020-06-28 16:16:41 -0500

    Section 2 Complete

---
+++ Tutorial04.R
@@ -0,0 +1,16 @@
+
+
+while(TRUE) {
+  print("Hello")
+}
+# press ESC on keyboard to stop processing
+
+counter <- 1
+while(counter < 12) {
+  print(counter)
+  counter <- counter + 1
+}
+
+for(i in 1:5){
+  print("Hello R")
+}
